9ef434c8f40479219b6f3834f09935085307227a
KRUTHIKA-M <kruthikam21@gmail.com>
2020-05-11 14:32:55 +0530

    first commit

--- src/main/java/com/example/devopscasestudy/api/ProductApi.java
+++ src/main/java/com/example/devopscasestudy/api/ProductApi.java
@@ -2,52 +2,68 @@ package com.example.devopscasestudy.api;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.devopscasestudy.model.Product;
 import com.example.devopscasestudy.repo.ProductRepository;
 
-@RestController  //rest api
+@RestController //used to create RESTApi
 public class ProductApi {
-    @Autowired             //look for productrepository obj and inject it here
-    private ProductRepository productRepository;
-    
-    
-    //node expess js
-    //app.get('/products',(req,res)=>)
-    
-    @GetMapping("/products")
-    public ResponseEntity<List<Product>> findAll(){
-        List<Product> products=productRepository.findAll();
-        return new ResponseEntity<List<Product>>(products,HttpStatus.OK);
+	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired 
+	private ProductRepository productRepository;
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>> findAll(){
+		logger.info("Processing findAll request");
+		List<Product> products=productRepository.findAll();
+		return new ResponseEntity<>(products, HttpStatus.OK);
+	}
+	
+	@PostMapping("/products")
+	public ResponseEntity<Product> save(@RequestBody Product product){
+		productRepository.save(product);
+		return new ResponseEntity<>(product, HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/products/bulkcreate")
+	public ResponseEntity<List<Product>> saveAll(@RequestBody List <Product> product){
+		productRepository.saveAll(product);
+		return new ResponseEntity<>(product,HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/products/{price}")
+	public ResponseEntity<List<Product>> findByPrice(@PathVariable("price")double price){
+		List<Product> products=productRepository.findByPriceGreaterThan(price);
+		return new ResponseEntity<>(products, HttpStatus.OK);
+	}
+	
+	@GetMapping("/products/find/{name}")
+	public ResponseEntity<List<Product>> findByName(@PathVariable("name")String name){
+		logger.info("Processing findByName request");
+		List<Product> products=productRepository.findByProductNameIgnoreCase(name);
+		return new ResponseEntity<>(products, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/products/{productId}")
+    public @ResponseBody ResponseEntity<StringResponse> delete(@PathVariable("productId")int productId){
+    productRepository.deleteById(productId);
+    return new ResponseEntity<>(new StringResponse("Deleted Order "+productId), HttpStatus.OK);
+   
     }
-    
-    //http://localhost:8081/products/567
-    @GetMapping("/products/{price}")
-    public ResponseEntity<List<Product>> findByPrice(@PathVariable("price") double price){
-        List<Product> products=productRepository.findByPriceGreaterThan(price);
-        return new ResponseEntity<List<Product>>(products,HttpStatus.OK);
-    }
-    
-    @GetMapping("/products/find/{name}")
-    public ResponseEntity<List<Product>> findByName(@PathVariable("name") String name){
-        List<Product> products=productRepository.findByProductNameIgnoreCase(name);
-        return new ResponseEntity<List<Product>>(products,HttpStatus.OK);
-    }
-    
-    //this is a string//
-    
-    //app.post('/products',(req,res)=>)
-    @PostMapping("/products")
-    public ResponseEntity<Product> save( @RequestBody Product product){
-        productRepository.save(product);
-        return new ResponseEntity<Product>(product, HttpStatus.CREATED);
-    }
+	
+	
 }
--- src/main/java/com/example/devopscasestudy/repo/ProductRepository.java
+++ src/main/java/com/example/devopscasestudy/repo/ProductRepository.java
@@ -2,7 +2,6 @@ package com.example.devopscasestudy.repo;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +11,7 @@ import com.example.devopscasestudy.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product ,Integer> {
 	List<Product> findByPriceGreaterThan(double price);
-	
+	List<Product> deleteById(int productId);
+	List<Product> findById(int productId);
 	List<Product> findByProductNameIgnoreCase(String productName);
 }
